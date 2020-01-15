738d24c46ebc45e9e97865a4e9dd8054ef4ad9c2
David Cid <david.cid@gmail.com>
2020-01-15 16:35:29 +0100

    created price objects

--- js/script.js
+++ js/script.js
@@ -4,22 +4,34 @@ const basicPrice = document.querySelector(".basic .price");
 const professionalPrice = document.querySelector(".professional .price");
 const masterPrice = document.querySelector(".master .price");
 
-const basic = 19.99;
-const professional = 24.99;
-const master = 39.99;
+const prices = {
+  basic: 19.99,
+  professional: 24.99,
+  master: 39.99
+}
 
 let monthly = false;
+
+const updatePrices = Object.values(prices).forEach(price => {
+  price *= 10;
+  console.log(price);
+});
 
 switcher.addEventListener("click", () => {
   ball.classList.toggle("monthly");
   if (!monthly) {
-    console.log("hey!");
+    basicPrice.innerHTML = `<span>&dollar;</span>${prices.basic}`;
+    professionalPrice.innerHTML = `<span>&dollar;</span>${prices.professional}`;
+    masterPrice.innerHTML = `<span>&dollar;</span>${prices.master}`;
     monthly = true;
   } else {
+    basicPrice.innerHTML = `<span>&dollar;</span>${prices.basic * 10}`;
+    professionalPrice.innerHTML = `<span>&dollar;</span>${prices.professional * 10}`;
+    masterPrice.innerHTML = `<span>&dollar;</span>${prices.master * 10}`;
     monthly = false;
   }
 });
 
-basicPrice.innerHTML = `<span>&dollar;</span>${basic}`;
-professionalPrice.innerHTML = `<span>&dollar;</span>${professional}`;
-masterPrice.innerHTML = `<span>&dollar;</span>${master}`;
+basicPrice.innerHTML = `<span>&dollar;</span>${prices.basic}`;
+professionalPrice.innerHTML = `<span>&dollar;</span>${prices.professional}`;
+masterPrice.innerHTML = `<span>&dollar;</span>${prices.master}`;
