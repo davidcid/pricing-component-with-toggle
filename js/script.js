3e018398a36428c498d6803c7d408f9379f13ad9
David Cid <david.cid@gmail.com>
2020-01-15 17:36:45 +0100

    finished js logic

--- js/script.js
+++ js/script.js
@@ -1,37 +1,39 @@
 const switcher = document.querySelector(".switch");
 const ball = document.querySelector(".ball");
-const basicPrice = document.querySelector(".basic .price");
-const professionalPrice = document.querySelector(".professional .price");
-const masterPrice = document.querySelector(".master .price");
+const priceBasic = document.querySelector(".price.basic");
+const priceProfessional = document.querySelector(".price.professional");
+const priceMaster = document.querySelector(".price.master");
 
-const prices = {
+const monthlyPlans = {
   basic: 19.99,
   professional: 24.99,
   master: 39.99
-}
+};
+
+const annualPlans = {
+  basic: 199.99,
+  professional: 249.99,
+  master: 399.99
+};
 
 let monthly = false;
 
-const updatePrices = Object.values(prices).forEach(price => {
-  price *= 10;
-  console.log(price);
-});
+function updatePrices() {
+  if (monthly) {
+    priceBasic.innerHTML = `<span>&dollar;</span>${monthlyPlans.basic}`;
+    priceProfessional.innerHTML = `<span>&dollar;</span>${monthlyPlans.professional}`;
+    priceMaster.innerHTML = `<span>&dollar;</span>${monthlyPlans.master}`;
+  } else {
+    priceBasic.innerHTML = `<span>&dollar;</span>${annualPlans.basic}`;
+    priceProfessional.innerHTML = `<span>&dollar;</span>${annualPlans.professional}`;
+    priceMaster.innerHTML = `<span>&dollar;</span>${annualPlans.master}`;
+  }
+}
+
+updatePrices();
 
 switcher.addEventListener("click", () => {
   ball.classList.toggle("monthly");
-  if (!monthly) {
-    basicPrice.innerHTML = `<span>&dollar;</span>${prices.basic}`;
-    professionalPrice.innerHTML = `<span>&dollar;</span>${prices.professional}`;
-    masterPrice.innerHTML = `<span>&dollar;</span>${prices.master}`;
-    monthly = true;
-  } else {
-    basicPrice.innerHTML = `<span>&dollar;</span>${prices.basic * 10}`;
-    professionalPrice.innerHTML = `<span>&dollar;</span>${prices.professional * 10}`;
-    masterPrice.innerHTML = `<span>&dollar;</span>${prices.master * 10}`;
-    monthly = false;
-  }
+  monthly ? (monthly = false) : (monthly = true);
+  updatePrices();
 });
-
-basicPrice.innerHTML = `<span>&dollar;</span>${prices.basic}`;
-professionalPrice.innerHTML = `<span>&dollar;</span>${prices.professional}`;
-masterPrice.innerHTML = `<span>&dollar;</span>${prices.master}`;
